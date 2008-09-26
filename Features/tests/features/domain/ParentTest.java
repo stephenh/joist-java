239b776f355cb29992bb66577a7df9a9b8e4873c
Stephen Haberman <stephen@exigencecorp.com>
2008-09-26 15:31:36 -0500

    Make sure object cache works.

--- Features/tests/features/domain/ParentTest.java
+++ Features/tests/features/domain/ParentTest.java
@@ -1,11 +1,12 @@
 package features.domain;
 
 import junit.framework.Assert;
+import features.domain.mappers.ChildMapper;
 import features.domain.mappers.ParentMapper;
 
 public class ParentTest extends AbstractFeaturesTest {
 
-    public void testSaveAndReloadChildInSeparateUoWThanParent() {
+    public void testLoadChildren() {
         Parent p = new Parent("parent");
         new Child(p, "child1");
         new Child(p, "child2");
@@ -17,6 +18,24 @@ public class ParentTest extends AbstractFeaturesTest {
         Assert.assertEquals("child2", p.getChilds().get(1).getName());
 
         Assert.assertTrue(p.getChilds() == p.getChilds());
+    }
+
+    public void testChildrenArrayAndThenMapperLoadIsTheSame() {
+        Parent p = new Parent("parent");
+        new Child(p, "child1");
+        new Child(p, "child2");
+        this.commitAndReOpen();
+
+        Assert.assertTrue(new ParentMapper().find(2).getChilds().get(0) == new ChildMapper().find(2));
+    }
+
+    public void testChildrenArrayAndAfterMapperLoadIsTheSame() {
+        Parent p = new Parent("parent");
+        new Child(p, "child1");
+        new Child(p, "child2");
+        this.commitAndReOpen();
+
+        Assert.assertTrue(new ChildMapper().find(2) == new ParentMapper().find(2).getChilds().get(0));
     }
 
 }
