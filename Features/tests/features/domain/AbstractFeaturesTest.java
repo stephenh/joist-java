4a9bb98325275a54284d61dcc85424e402df134d
Stephen Haberman <stephen@exigencecorp.com>
2008-10-24 22:58:29 -0500

    Drop FlushDatabase/Schema in favor of flush_test_database() call.

--- Features/tests/features/domain/AbstractFeaturesTest.java
+++ Features/tests/features/domain/AbstractFeaturesTest.java
@@ -1,21 +1,19 @@
 package features.domain;
 
 import org.exigencecorp.domainobjects.AbstractDomainObjectsTest;
-import org.exigencecorp.domainobjects.util.FlushDatabase;
+import org.exigencecorp.jdbc.Jdbc;
 
 import features.Registry;
 
 public abstract class AbstractFeaturesTest extends AbstractDomainObjectsTest {
 
-    private static final FlushDatabase flushDatabase;
     static {
         Registry.getApplicationInstance().start();
-        flushDatabase = new FlushDatabase(Registry.getDataSource());
     }
 
     public void setUp() throws Exception {
         super.setUp();
-        AbstractFeaturesTest.flushDatabase.flush();
+        Jdbc.queryForInt(Registry.getDataSource(), "SELECT flush_test_database()");
     }
 
 }
