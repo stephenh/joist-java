9cfb35a143dc5a2630dba81c4473684d21db6358
Stephen Haberman <stephen@exigencecorp.com>
2008-11-12 16:18:02 -0600

    Use load to hit the IdentityMap first.

--- DomainObjects/src/org/exigencecorp/domainobjects/AbstractQueries.java
+++ DomainObjects/src/org/exigencecorp/domainobjects/AbstractQueries.java
@@ -1,8 +1,6 @@
 package org.exigencecorp.domainobjects;
 
-import org.exigencecorp.domainobjects.orm.AliasRegistry;
-import org.exigencecorp.domainobjects.queries.Alias;
-import org.exigencecorp.domainobjects.queries.Select;
+import org.exigencecorp.domainobjects.uow.UoW;
 
 public abstract class AbstractQueries<T extends DomainObject> {
 
@@ -13,12 +11,8 @@ public abstract class AbstractQueries<T extends DomainObject> {
     }
 
     public T find(Integer id) {
-        Alias<T> alias = this.getAlias();
-        return Select.from(alias).where(alias.getIdColumn().equals(id)).unique();
-    }
-
-    private Alias<T> getAlias() {
-        return AliasRegistry.get(this.domainType);
+        // Use load as it hits the IdentityMap and could avoid an unneeded query
+        return UoW.getCurrent().getRepository().load(this.domainType, id);
     }
 
 }
