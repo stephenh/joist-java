eb077f8d741c3324cad6df29041253de5a4e938c
Stephen Haberman <stephen@exigencecorp.com>
2008-12-06 11:20:05 -0600

    A bd.bat to refresh the utility jar.

---
+++ DomainObjects/bd.bat
@@ -0,0 +1,6 @@
+@echo off
+if "%JAVA_HOME%"=="" echo Error: JAVA_HOME is not defined.
+if not exist bin mkdir bin
+if not exist bin\bd mkdir bin\bd
+"%JAVA_HOME%/bin/javac" -cp "lib/bd/bd.jar" -sourcepath src/bd -d bin/bd src/bd/*.java
+"%JAVA_HOME%/bin/java" -Xmx512m -cp "bin/bd;lib/bd/bd.jar;%JAVA_HOME%/lib/tools.jar" org.exigencecorp.bd.BuildRunner %*
