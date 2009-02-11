d6613bd977cb6f05cfaf8de6d858c014ef3f395f
Stephen Haberman <stephen@exigencecorp.com>
2009-02-11 13:23:46 -0600

    Upgrade bd.

--- DomainObjects/src/bd/Build.java
+++ DomainObjects/src/bd/Build.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 import org.exigencecorp.bd.Zip;
 import org.exigencecorp.bd.java.Jar;
 import org.exigencecorp.bd.java.Lib;
@@ -6,12 +8,18 @@ import org.exigencecorp.bd.java.Source;
 public class Build {
 
     public Lib libMain = new Lib("lib/main");
-    public Source srcMain = new Source("src/main", "bin/main").lib(this.libMain);
-    public Jar binaryJar = new Jar("bin/exigence-domainobjects.jar").includes(this.srcMain.getDestination());
-    public Zip sourceZip = new Zip("bin/exigence-domainobjects.zip").includes(this.srcMain.getSourceFiles());
+    public Lib libBd = new Lib("lib/bd");
+    public File binMain = new File("bin/main");
+    public Source srcMain = new Source("src/main", this.binMain).lib(this.libMain);
+    public Jar binaryJar = new Jar("bin/exigence-domainobjects.jar").includes(this.binMain);
+    public Zip sourceZip = new Zip("bin/exigence-domainobjects.zip").includes(this.srcMain.getSourceFiles().get(0));
 
     public Build() {
+    }
+
+    public void updateLibs() {
         this.libMain.updateFromHomeCache();
+        this.libBd.updateFromHomeCache();
     }
 
     public void compile() {
