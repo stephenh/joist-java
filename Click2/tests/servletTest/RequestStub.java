712a6faa3b4629babdfa0c249653e79cbbd19cc0
Stephen Haberman <stephen@exigencecorp.com>
2009-03-02 17:17:13 -0600

    Remove todo stub comments.

--- Click2/tests/servletTest/RequestStub.java
+++ Click2/tests/servletTest/RequestStub.java
@@ -25,42 +25,34 @@ public class RequestStub implements HttpServletRequest {
     private Map<String, String[]> parameters = new HashMap<String, String[]>();
 
     public String getAuthType() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getContextPath() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Cookie[] getCookies() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public long getDateHeader(String arg0) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public String getHeader(String arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Enumeration<String> getHeaderNames() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Enumeration<String> getHeaders(String arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public int getIntHeader(String arg0) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
@@ -73,22 +65,18 @@ public class RequestStub implements HttpServletRequest {
     }
 
     public String getPathInfo() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getPathTranslated() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getQueryString() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getRemoteUser() {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -101,12 +89,10 @@ public class RequestStub implements HttpServletRequest {
     }
 
     public StringBuffer getRequestURL() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getRequestedSessionId() {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -115,97 +101,78 @@ public class RequestStub implements HttpServletRequest {
     }
 
     public HttpSession getSession() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public HttpSession getSession(boolean arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Principal getUserPrincipal() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public boolean isRequestedSessionIdFromCookie() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public boolean isRequestedSessionIdFromURL() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public boolean isRequestedSessionIdFromUrl() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public boolean isRequestedSessionIdValid() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public boolean isUserInRole(String arg0) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public Object getAttribute(String arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Enumeration<String> getAttributeNames() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getCharacterEncoding() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public int getContentLength() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public String getContentType() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public ServletInputStream getInputStream() throws IOException {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getLocalAddr() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getLocalName() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public int getLocalPort() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public Locale getLocale() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Enumeration<Locale> getLocales() {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -235,70 +202,56 @@ public class RequestStub implements HttpServletRequest {
     }
 
     public String getProtocol() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public BufferedReader getReader() throws IOException {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getRealPath(String arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getRemoteAddr() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getRemoteHost() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public int getRemotePort() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public RequestDispatcher getRequestDispatcher(String arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getScheme() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getServerName() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public int getServerPort() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public boolean isSecure() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public void removeAttribute(String arg0) {
-        // TODO Auto-generated method stub
     }
 
     public void setAttribute(String arg0, Object arg1) {
-        // TODO Auto-generated method stub
     }
 
     public void setCharacterEncoding(String arg0) throws UnsupportedEncodingException {
-        // TODO Auto-generated method stub
     }
 
     public void setServletPath(String servletPath) {
--- Click2/tests/servletTest/ResponseStub.java
+++ Click2/tests/servletTest/ResponseStub.java
@@ -19,52 +19,41 @@ public class ResponseStub implements HttpServletResponse {
     }
 
     public void addCookie(Cookie arg0) {
-        // TODO Auto-generated method stub
     }
 
     public void addDateHeader(String arg0, long arg1) {
-        // TODO Auto-generated method stub
     }
 
     public void addHeader(String arg0, String arg1) {
-        // TODO Auto-generated method stub
     }
 
     public void addIntHeader(String arg0, int arg1) {
-        // TODO Auto-generated method stub
     }
 
     public boolean containsHeader(String arg0) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public String encodeRedirectURL(String arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String encodeRedirectUrl(String arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String encodeURL(String arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String encodeUrl(String arg0) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void sendError(int arg0) throws IOException {
-        // TODO Auto-generated method stub
     }
 
     public void sendError(int arg0, String arg1) throws IOException {
-        // TODO Auto-generated method stub
     }
 
     public String getRedirect() {
@@ -76,46 +65,36 @@ public class ResponseStub implements HttpServletResponse {
     }
 
     public void setDateHeader(String arg0, long arg1) {
-        // TODO Auto-generated method stub
     }
 
     public void setHeader(String arg0, String arg1) {
-        // TODO Auto-generated method stub
     }
 
     public void setIntHeader(String arg0, int arg1) {
-        // TODO Auto-generated method stub
     }
 
     public void setStatus(int arg0) {
-        // TODO Auto-generated method stub
     }
 
     public void setStatus(int arg0, String arg1) {
-        // TODO Auto-generated method stub
     }
 
     public void flushBuffer() throws IOException {
-        // TODO Auto-generated method stub
     }
 
     public int getBufferSize() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public String getCharacterEncoding() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String getContentType() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Locale getLocale() {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -132,38 +111,30 @@ public class ResponseStub implements HttpServletResponse {
     }
 
     public boolean isCommitted() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public void reset() {
-        // TODO Auto-generated method stub
 
     }
 
     public void resetBuffer() {
-        // TODO Auto-generated method stub
 
     }
 
     public void setBufferSize(int arg0) {
-        // TODO Auto-generated method stub
     }
 
     public void setCharacterEncoding(String arg0) {
-        // TODO Auto-generated method stub
     }
 
     public void setContentLength(int arg0) {
-        // TODO Auto-generated method stub
     }
 
     public void setContentType(String arg0) {
-        // TODO Auto-generated method stub
     }
 
     public void setLocale(Locale arg0) {
-        // TODO Auto-generated method stub
     }
 
 }
