90a974a8fc6ecf8087f56511e7859f342c3722e9
Stephen Haberman <stephen@exigencecorp.com>
2009-03-06 22:42:08 -0600

    I don't have time to screw around with bd right now--just use ant.

--- bd.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if "%JAVA_HOME%"=="" echo Error: JAVA_HOME is not defined.
-if not exist bin mkdir bin
-if not exist bin\bd mkdir bin\bd
-"%JAVA_HOME%/bin/javac" -cp "lib/bd/*" -sourcepath src/bd -d bin/bd src/bd/*.java
-"%JAVA_HOME%/bin/java" -Xmx512m -cp "bin/bd;lib/bd/*;%JAVA_HOME%/lib/tools.jar" org.exigencecorp.bd.BuildRunner %*
