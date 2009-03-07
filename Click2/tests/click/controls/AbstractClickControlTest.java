b09d07053303c1bebc4fb25208c7fe2814547011
Stephen Haberman <stephen@exigencecorp.com>
2009-03-07 14:27:30 -0600

    Create just one VelocityEngine instead of one/test.

--- Click2/tests/click/controls/AbstractClickControlTest.java
+++ Click2/tests/click/controls/AbstractClickControlTest.java
@@ -2,20 +2,34 @@ package click.controls;
 
 import java.io.StringWriter;
 
+import org.apache.velocity.app.VelocityEngine;
+
 import click.AbstractClickTest;
 import click.ClickConfig;
 import click.ClickContext;
 import click.Control;
 import click.CurrentContext;
+import click.fakedomain.EmployeeToStringConverter;
 import click.util.HtmlWriter;
 
 public abstract class AbstractClickControlTest extends AbstractClickTest {
 
+    private static VelocityEngine cachedEngine;
     protected ClickConfig config;
 
     public void setUp() throws Exception {
         super.setUp();
-        this.config = new ClickConfig("click.pages");
+        this.config = new ClickConfig("click.pages") {
+            @Override
+            protected VelocityEngine createVelocityEngine() {
+                // Creating a new engine for each test was causing heap errors
+                if (AbstractClickControlTest.cachedEngine == null) {
+                    AbstractClickControlTest.cachedEngine = super.createVelocityEngine();
+                }
+                return AbstractClickControlTest.cachedEngine;
+            }
+        };
+        this.config.getUrlConverterRegistry().addConverter(new EmployeeToStringConverter());
         CurrentContext.set(new ClickContext(null, this.config, null, null));
     }
 
