4615bb5d5be4882565b1cb21a13acaf93ae90ba6
Stephen Haberman <stephen@exigencecorp.com>
2009-03-07 19:10:39 -0600

    Enforce convertTwoToOne is unsupported as final.

--- src/main/org/exigencecorp/conversion/AbstractOneWayConverter.java
+++ src/main/org/exigencecorp/conversion/AbstractOneWayConverter.java
@@ -2,7 +2,7 @@ package org.exigencecorp.conversion;
 
 public abstract class AbstractOneWayConverter<T, U> extends AbstractConverter<T, U> {
 
-    public T convertTwoToOne(U value, Class<? extends T> toType) {
+    public final T convertTwoToOne(U value, Class<? extends T> toType) {
         throw new UnsupportedConversionException(value, toType);
     }
 
