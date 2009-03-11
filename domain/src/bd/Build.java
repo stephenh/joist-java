7bc5ebf331dfb3caf34c9b9eee522650953b8d12
Stephen Haberman <stephen@exigencecorp.com>
2009-03-11 14:22:26 -0500

    Update bd to output joist.domain.jar/zip.

--- domain/src/bd/Build.java
+++ domain/src/bd/Build.java
@@ -1,5 +1,6 @@
 import java.io.File;
 
+import org.exigencecorp.bd.Files;
 import org.exigencecorp.bd.Zip;
 import org.exigencecorp.bd.java.Jar;
 import org.exigencecorp.bd.java.Lib;
@@ -7,12 +8,13 @@ import org.exigencecorp.bd.java.Source;
 
 public class Build {
 
+    public Files bin = new Files("bin");
+    public File binMain = new File("bin/main");
     public Lib libMain = new Lib("lib/main");
     public Lib libBd = new Lib("lib/bd");
-    public File binMain = new File("bin/main");
     public Source srcMain = new Source("src/main", this.binMain).lib(this.libMain);
-    public Jar binaryJar = new Jar("bin/exigence-domainobjects.jar").includes(this.binMain);
-    public Zip sourceZip = new Zip("bin/exigence-domainobjects.zip").includes(this.srcMain.getSourceFiles().get(0));
+    public Jar binaryJar = new Jar("bin/joist.domain.jar").includes(this.binMain);
+    public Zip sourceZip = new Zip("bin/joist.domain.zip").includes(this.srcMain.getSourceFiles().get(0));
 
     public Build() {
     }
@@ -20,6 +22,10 @@ public class Build {
     public void updateLibs() {
         this.libMain.updateFromHomeCache();
         this.libBd.updateFromHomeCache();
+    }
+
+    public void clean() {
+        this.bin.delete();
     }
 
     public void compile() {
