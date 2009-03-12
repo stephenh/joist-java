a8030dc8826ba6da431410ae304861b173459641
Stephen Haberman <stephen@exigencecorp.com>
2009-03-11 21:57:08 -0500

    I heard that later versions of pg dislike deferred constraints in transactions that change the schema.
    I'm not sure why we originally had this anyway, so just remove it.

--- domain/src/main/joist/domain/migrations/MigraterConfig.java
+++ domain/src/main/joist/domain/migrations/MigraterConfig.java
@@ -8,7 +8,7 @@ public class MigraterConfig {
     public List<String> packageNamesContainingMigrations = new ArrayList<String>();
 
     public String getInitialConnectionSetupCommand() {
-        return "SET CONSTRAINTS ALL DEFERRED; SET CLIENT_ENCODING TO 'UNICODE';";
+        return null;
     }
 
     public void setProjectNameForDefaults(String projectName) {
