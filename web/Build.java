1dd057b514e318955f538777000d47bc2b38c1a1
Stephen Haberman <stephen@exigencecorp.com>
2009-03-14 11:14:49 -0500

    Update for at lesat compile to work. Not doing tests yet.

--- web/Build.java
+++ web/Build.java
@@ -3,19 +3,21 @@ import java.io.File;
 import org.exigencecorp.bd.resources.Dir;
 import org.exigencecorp.bd.resources.Zip;
 import org.exigencecorp.bd.resources.java.Jar;
-import org.exigencecorp.bd.resources.java.Lib;
 import org.exigencecorp.bd.resources.java.Source;
 
 public class Build {
 
+    static {
+        Source.defaultIncludeHomeCache = true;
+    }
+
     public Dir bin = new Dir("bin");
     public Dir binMain = this.bin.dir("main");
-    
-    public Lib libMain = new Lib("lib/main");
-    public Lib libBd = new Lib("lib/bd");
-    public Lib libHomeCache = Lib.homeCache();
-    
-    public Source srcMain = new Source("src", this.binMain).lib(this.libMain).lib(this.libHomeCache);
+
+    public Dir libMain = new Dir("lib/main");
+    public Dir libBd = new Dir("lib/bd");
+
+    public Source srcMain = new Source("src", this.binMain).lib(this.libMain);
     public Jar binaryJar = new Jar("bin/joist.web.jar").includes(this.binMain);
     public Zip sourceZip = new Zip("bin/joist.web.zip").includes(this.srcMain.getSources());
 
@@ -23,8 +25,8 @@ public class Build {
     }
 
     public void updateLibs() {
-        this.libMain.updateFromHomeCache();
-        this.libBd.updateFromHomeCache();
+        // this.libMain.updateFromHomeCache();
+        // this.libBd.updateFromHomeCache();
     }
 
     public void compile() {
