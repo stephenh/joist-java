dc114f29cea24444366f6b86cf036a48e0a0d12f
Stephen Haberman <stephen@exigencecorp.com>
2009-03-23 16:09:36 -0500

    util: Use a ConcurrentHashMap-based Memoizer instead of synchronized.

--- util/src/main/joist/registry/ResourceBag.java
+++ util/src/main/joist/registry/ResourceBag.java
@@ -1,27 +1,27 @@
 package joist.registry;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
+import joist.util.Memoizer;
 import joist.util.Reflection;
+import joist.util.Memoizer.Computable;
 
 /** A bag for holding resources as pseudo-singletons that can later be destroyed. */
 public class ResourceBag {
 
-    private Map<Class<?>, Object> instancesByFactoryClass = new LinkedHashMap<Class<?>, Object>();
+    private final Computable<Class<?>, Object> computer = new Computable<Class<?>, Object>() {
+        public Object compute(Class<?> arg) throws InterruptedException {
+            return ((ResourceFactory<?>) Reflection.newInstance(arg)).create();
+        }
+    };
+    private final Memoizer<Class<?>, Object> instancesByFactoryClass = new Memoizer<Class<?>, Object>(this.computer);
 
     /** @return an instance created by <code>new FactoryClass().create()</code>, 1 per <code>factoryClass</code> class (singleton) */
-    public synchronized <T, U extends ResourceFactory<T>> T get(Class<U> factoryClass) {
-        T instance = (T) this.instancesByFactoryClass.get(factoryClass);
-        if (instance == null) {
-            U factory = Reflection.newInstance(factoryClass);
-            instance = factory.create();
-            this.instancesByFactoryClass.put(factoryClass, instance);
-        }
-        return instance;
+    public <T, U extends ResourceFactory<T>> T get(final Class<U> factoryClass) {
+        return (T) this.instancesByFactoryClass.compute(factoryClass);
     }
 
-    public synchronized <T, U extends ResourceFactory<T>> LazyResource<T> getLazy(final Class<U> factoryClass) {
+    public <T, U extends ResourceFactory<T>> LazyResource<T> getLazy(final Class<U> factoryClass) {
         return new LazyResource<T>() {
             public T get() {
                 return ResourceBag.this.get(factoryClass);
@@ -30,7 +30,7 @@ public class ResourceBag {
     }
 
     /** Calls <code>destroy</code> for all the resources created by factories. */
-    public synchronized void destroyResources() {
+    public void destroyResources() {
         for (Map.Entry<Class<?>, Object> entry : this.instancesByFactoryClass.entrySet()) {
             ResourceFactory<Object> factory = (ResourceFactory<Object>) Reflection.newInstance(entry.getKey());
             factory.destroy(entry.getValue());
