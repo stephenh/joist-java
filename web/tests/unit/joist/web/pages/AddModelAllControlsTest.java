4047850486077b747442ff4020c7b9155b5fabd2
Stephen Haberman <stephen@exigencecorp.com>
2009-03-24 15:52:23 -0500

    web: Add a test showing all controls getting added to the model.

---
+++ web/tests/unit/joist/web/pages/AddModelAllControlsTest.java
@@ -0,0 +1,18 @@
+package joist.web.pages;
+
+import junit.framework.Assert;
+
+public class AddModelAllControlsTest extends AbstractClickPageTest {
+
+    public void testFormOnlyInOnInit() throws Exception {
+        String body = this.request("/addModelAllControls.htm").getBody();
+        Assert.assertTrue(body.contains("<p class=\"clickFormHeading\">Foo</p>"));
+    }
+
+    public void testFormFieldOnlyInOnInit() throws Exception {
+        String body = this.request("/addModelAllControls.htm").set("onlyField", "true").getBody();
+        // Assert.assertEquals("<input id=\"foo-bar\" name=\"bar\" type=\"text\" value=\"\"/>\r\n", body);
+        Assert.assertEquals("${foo-bar}\r\n", body);
+    }
+
+}
