82710dac166f2b8a62e06cdb4bc5075173b5f9ae
Stephen Haberman <stephen@exigencecorp.com>
2009-03-27 15:06:52 -0500

    web: AbstractLayoutControl extends AbstractContainer and gets a default id.

--- web/src/main/joist/web/AbstractLayoutControl.java
+++ web/src/main/joist/web/AbstractLayoutControl.java
@@ -1,9 +1,14 @@
 package joist.web;
 
+import joist.util.Inflector;
 import joist.web.util.HtmlWriter;
 import joist.web.util.VelocityRenderer;
 
-public abstract class AbstractLayoutControl extends AbstractControl {
+public abstract class AbstractLayoutControl extends AbstractContainer {
+
+    public AbstractLayoutControl() {
+        this.setId(Inflector.uncapitalize(this.getClass().getSimpleName()));
+    }
 
     public void render(HtmlWriter w) {
         VelocityRenderer.render(this, w);
