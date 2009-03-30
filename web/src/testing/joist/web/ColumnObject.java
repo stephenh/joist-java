33ab913a1d3f1111cbbaded618b5b05b16012db1
Stephen Haberman <stephen@exigencecorp.com>
2009-03-30 13:32:06 -0500

    web: Add a ColumnObject prototype.

---
+++ web/src/testing/joist/web/ColumnObject.java
@@ -0,0 +1,21 @@
+package joist.web;
+
+public class ColumnObject {
+
+    protected final AbstractPageObject pageObject;
+    protected final String id;
+
+    public ColumnObject(AbstractPageObject pageObject, String id) {
+        this.pageObject = pageObject;
+        this.id = id;
+    }
+
+    public String get(int index) {
+        return this.pageObject.getTextOrNull(this.id + "-" + index);
+    }
+
+    public void click(int index) {
+        this.pageObject.click(this.id + "-" + index + "-link");
+    }
+
+}
