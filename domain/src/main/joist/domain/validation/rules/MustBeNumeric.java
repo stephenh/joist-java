b65a5129724d9ef82ab7a32e27531a0cc1a0ac36
Stephen Haberman <stephen@exigencecorp.com>
2009-03-30 11:53:03 -0500

    domain: MustBeNumeric uses Shim.getName().

--- domain/src/main/joist/domain/validation/rules/MustBeNumeric.java
+++ domain/src/main/joist/domain/validation/rules/MustBeNumeric.java
@@ -8,18 +8,20 @@ import org.apache.commons.lang.StringUtils;
 
 public class MustBeNumeric<T extends AbstractDomainObject> implements Rule<T> {
 
-    private final String property;
     private final Shim<T, String> shim;
 
-    public MustBeNumeric(String property, Shim<T, String> shim) {
-        this.property = property;
+    public static <T extends AbstractDomainObject> MustBeNumeric<T> rule(Shim<T, String> shim) {
+        return new MustBeNumeric<T>(shim);
+    }
+
+    public MustBeNumeric(Shim<T, String> shim) {
         this.shim = shim;
     }
 
     public void validate(ValidationErrors errors, T t) {
         String value = this.shim.get(t);
         if (value != null && !StringUtils.isNumeric(value)) {
-            errors.addPropertyError(t, this.property, "must be numeric");
+            errors.addPropertyError(t, this.shim.getName(), "must be numeric");
         }
     }
 
