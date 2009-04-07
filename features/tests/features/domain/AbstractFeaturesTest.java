0f7f78f6ff1d805030c10a2b2c941a17cebc223c
Stephen Haberman <stephen@exigencecorp.com>
2009-04-07 15:11:13 -0500

    features: Use H2's script/runscript commands for backup.

--- features/tests/features/domain/AbstractFeaturesTest.java
+++ features/tests/features/domain/AbstractFeaturesTest.java
@@ -1,28 +1,68 @@
 package features.domain;
 
+import java.io.File;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import joist.domain.AbstractDomainObjectsTest;
 import joist.domain.orm.AliasRegistry;
 import joist.domain.orm.queries.Alias;
+import joist.jdbc.Jdbc;
+import joist.jdbc.RowMapper;
 
 import org.h2.engine.Session;
 import org.h2.jdbc.JdbcConnection;
 import org.h2.schema.Schema;
+
+import com.vladium.utils.timing.ITimer;
+import com.vladium.utils.timing.TimerFactory;
 
 import features.Registry;
 import features.cli.JoistCli;
 
 public abstract class AbstractFeaturesTest extends AbstractDomainObjectsTest {
 
-    static {
-        Registry.start();
-        JoistCli c = new JoistCli();
-        c.migrateDatabase();
-    }
+    private static boolean started = false;
+    private static File backup = new File("./bin/backup.sql");
 
     public void setUp() throws Exception {
+        if (!started) {
+            Registry.start();
+
+            if (backup.exists()) {
+                ITimer t = TimerFactory.newTimer();
+                t.start();
+
+                Connection conn = Registry.getDataSource().getConnection();
+                Jdbc.update(conn, "RUNSCRIPT FROM '{}'", backup.getAbsolutePath());
+                conn.close();
+
+                t.stop();
+                System.out.println("Setup Took " + t.getDuration() + "ms");
+            } else {
+                ITimer t = TimerFactory.newTimer();
+                t.start();
+
+                new JoistCli().migrateDatabase();
+
+                t.stop();
+                System.out.println("Setup Took " + t.getDuration() + "ms");
+                Connection conn = Registry.getDataSource().getConnection();
+                Jdbc.query(conn, "SCRIPT TO '" + backup.getAbsolutePath() + "'", new RowMapper() {
+                    public void mapRow(ResultSet rs) throws SQLException {
+                        // System.out.println(rs.getObject(1));
+                    }
+                });
+                conn.close();
+            }
+            started = true;
+        }
+
         super.setUp();
+
+        ITimer t = TimerFactory.newTimer();
+        t.start();
 
         Connection c = Registry.getDataSource().getConnection();
         Session s = (Session) ((JdbcConnection) c).getSession();
@@ -37,6 +77,9 @@ public abstract class AbstractFeaturesTest extends AbstractDomainObjectsTest {
         } finally {
             c.close();
         }
+
+        t.stop();
+        System.out.println("Flush Took " + t.getDuration() + "ms");
     }
 
 }
