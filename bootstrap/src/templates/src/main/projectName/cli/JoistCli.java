bfb53a268ec20f921e6debbe5a1cef38c66b2b8f
Stephen Haberman <stephen@exigencecorp.com>
2009-04-20 14:56:29 -0500

    bootstrap: Fix some projectName vs. project_name references.

--- bootstrap/src/templates/src/main/projectName/cli/JoistCli.java
+++ bootstrap/src/templates/src/main/projectName/cli/JoistCli.java
@@ -5,7 +5,7 @@ import joist.domain.codegen.AbstractJoistCli;
 public class JoistCli extends AbstractJoistCli {
 
     public JoistCli() {
-        super("@projectName@");
+        super("@project_name@");
     }
 
 }
