57b19cfe2b67731ad0c446795195a12608a870bb
Stephen Haberman <stephen@exigencecorp.com>
2009-07-21 11:30:14 -0500

    features: Use a private static rule.

--- features/src/main/features/domain/ValidationAFoo.java
+++ features/src/main/features/domain/ValidationAFoo.java
@@ -3,31 +3,30 @@ package features.domain;
 import joist.domain.util.TextString;
 import joist.domain.validation.ValidationErrors;
 import joist.domain.validation.rules.Rule;
+
 import org.exigencecorp.bindgen.Bindable;
 
 @Bindable
 public class ValidationAFoo extends ValidationAFooCodegen {
 
+    private static final Rule<ValidationAFoo> customRule = new Rule<ValidationAFoo>() {
+        public void validate(ValidationErrors errors, ValidationAFoo foo) {
+            if ("bar".equals(foo.getName())) {
+                errors.addPropertyError(foo, "name", "must not be bar");
+            }
+            if ("baz".equals(foo.getName())) {
+                errors.addObjectError(foo, "is all messed up");
+            }
+        }
+    };
+
     public ValidationAFoo() {
-        this.addExtraRules();
+        this.addRule(customRule);
     }
 
     @Override
     public String toTextString() {
         return TextString.join(this.getName());
-    }
-
-    private void addExtraRules() {
-        this.addRule(new Rule<ValidationAFoo>() {
-            public void validate(ValidationErrors errors, ValidationAFoo foo) {
-                if ("bar".equals(foo.getName())) {
-                    errors.addPropertyError(foo, "name", "must not be bar");
-                }
-                if ("baz".equals(foo.getName())) {
-                    errors.addObjectError(foo, "is all messed up");
-                }
-            }
-        });
     }
 
 }
