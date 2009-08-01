70b8c28781c6001e7a04c248395a948df170d8b5
Stephen Haberman <stephen@exigencecorp.com>
2009-08-01 16:53:56 -0500

    Put the ref/stub stuff here for now.

--- domain/src/testing/joist/domain/AbstractDomainObjectsTest.java
+++ domain/src/testing/joist/domain/AbstractDomainObjectsTest.java
@@ -1,10 +1,16 @@
 package joist.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import joist.domain.uow.UoW;
+import joist.registry.Ref;
 import joist.util.TestCounters;
 import junit.framework.TestCase;
 
 public abstract class AbstractDomainObjectsTest extends TestCase {
+
+    private Map<Ref<? extends Object>, Object> stubs = new HashMap<Ref<? extends Object>, Object>();
 
     public void setUp() throws Exception {
         super.setUp();
@@ -18,6 +24,9 @@ public abstract class AbstractDomainObjectsTest extends TestCase {
 
     public void tearDown() throws Exception {
         UoW.close();
+        for (Map.Entry<Ref<? extends Object>, Object> entry : this.stubs.entrySet()) {
+            ((Ref<Object>) entry.getKey()).set(entry.getValue());
+        }
         super.tearDown();
     }
 
@@ -35,6 +44,11 @@ public abstract class AbstractDomainObjectsTest extends TestCase {
 
     protected <T extends DomainObject> T reload(T instance) {
         return (T) UoW.load(instance.getClass(), instance.getId());
+    }
+
+    protected <T> void stub(Ref<T> ref, T tempValue) {
+        this.stubs.put(ref, ref.get());
+        ref.set(tempValue);
     }
 
 }
