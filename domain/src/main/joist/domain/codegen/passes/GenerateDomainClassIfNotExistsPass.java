e144e6e9147692d07df6349987ccc41280ce1e84
Stephen Haberman <stephen@exigencecorp.com>
2009-10-06 22:39:09 -0500

    domain: Last of bindgen in domain.

--- domain/src/main/joist/domain/codegen/passes/GenerateDomainClassIfNotExistsPass.java
+++ domain/src/main/joist/domain/codegen/passes/GenerateDomainClassIfNotExistsPass.java
@@ -4,8 +4,6 @@ import joist.domain.codegen.Codegen;
 import joist.domain.codegen.dtos.Entity;
 import joist.sourcegen.GClass;
 
-import org.exigencecorp.bindgen.Bindable;
-
 public class GenerateDomainClassIfNotExistsPass implements Pass {
 
     public void pass(Codegen codegen) {
@@ -16,8 +14,6 @@ public class GenerateDomainClassIfNotExistsPass implements Pass {
             if (!codegen.getOutputSourceDirectory().exists(entity.getFullClassName())) {
                 GClass domain = codegen.getOutputSourceDirectory().getClass(entity.getFullClassName());
                 domain.baseClassName(entity.getFullCodegenClassName());
-                domain.addAnnotation("@Bindable");
-                domain.addImports(Bindable.class);
             }
         }
     }
