b3a6fbd34f2ea946a4f8e58b0cf231c931e6c859
Stephen Haberman <stephen@exigencecorp.com>
2009-10-09 10:36:40 -0500

    Add MoneyAliasColumn.

---
+++ domain/src/main/joist/domain/orm/queries/columns/MoneyAliasColumn.java
@@ -0,0 +1,33 @@
+package joist.domain.orm.queries.columns;
+
+import java.math.BigDecimal;
+
+import joist.domain.DomainObject;
+import joist.domain.Shim;
+import joist.domain.orm.queries.Alias;
+
+import com.domainlanguage.money.Money;
+
+public class MoneyAliasColumn<T extends DomainObject> extends AliasColumn<T, Money, Long> {
+
+    public MoneyAliasColumn(Alias<T> alias, String name, Shim<T, Money> shim) {
+        super(alias, name, shim);
+    }
+
+    @Override
+    public Money toDomainValue(Long jdbcValue) {
+        if (jdbcValue == null) {
+            return null;
+        }
+        return Money.dollars(jdbcValue.intValue() / 100);
+    }
+
+    @Override
+    public Long toJdbcValue(Money domainValue) {
+        if (domainValue == null) {
+            return null;
+        }
+        return domainValue.breachEncapsulationOfAmount().multiply(new BigDecimal(100)).toBigIntegerExact().longValue();
+    }
+
+}
