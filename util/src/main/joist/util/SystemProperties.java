80a83d74842514353bbabcffb7de0871be030eb9
Stephen Haberman <stephen@exigencecorp.com>
2009-10-27 21:23:29 -0500

    util: Fix containsKey and add stage overrides.

--- util/src/main/joist/util/SystemProperties.java
+++ util/src/main/joist/util/SystemProperties.java
@@ -8,20 +8,48 @@ import java.util.Map.Entry;
 
 public class SystemProperties {
 
+    /** Loads path into the system properties, without overwriting, and performing "*.stage" substitution. */
     public static void loadFromFileIfExists(String path) {
         if (!new File(path).exists()) {
             return;
         }
-
         Properties p = new Properties();
         try {
             p.load(new FileReader(path));
         } catch (IOException io) {
             throw new RuntimeException(io);
         }
+        String stage = System.getProperty("stage");
+        SystemProperties.overrideForSuffix(stage, p);
+        SystemProperties.setUnlessExists(p);
+    }
 
+    /** Loads path into the system properties, without overwriting, and performing "*.stage" substitution. */
+    public static void loadFromClasspath(String path) {
+        Properties p = new Properties();
+        try {
+            p.load(SystemProperties.class.getResourceAsStream(path));
+        } catch (IOException io) {
+            throw new RuntimeException(io);
+        }
+        String stage = System.getProperty("stage");
+        SystemProperties.overrideForSuffix(stage, p);
+        SystemProperties.setUnlessExists(p);
+    }
+
+    public static void overrideForSuffix(String suffix, Properties p) {
+        for (Entry<Object, Object> e : Copy.list(p.entrySet())) {
+            String key = (String) e.getKey();
+            if (key.endsWith("." + suffix)) {
+                String actualKey = key.substring(0, key.length() - ("." + suffix).length());
+                p.setProperty(actualKey, (String) e.getValue());
+            }
+        }
+    }
+
+    private static void setUnlessExists(Properties p) {
         for (Entry<Object, Object> e : p.entrySet()) {
-            if (!System.getProperties().contains(e.getKey())) {
+            if (!System.getProperties().containsKey(e.getKey())) {
                 System.setProperty((String) e.getKey(), (String) e.getValue());
             }
         }
