f05f4c98c1bd4ff26ef6594bd0d2cce636c1ac2d
Stephen Haberman <stephen@exigencecorp.com>
2010-01-16 01:50:42 -0600

    Avoid NPE in nullable codes.

--- domain/src/main/joist/domain/orm/ForeignKeyCodeHolder.java
+++ domain/src/main/joist/domain/orm/ForeignKeyCodeHolder.java
@@ -6,7 +6,7 @@ import joist.domain.util.Codes;
 /** A value holder that will lazy load the foreign key that points to a code/enum. */
 public class ForeignKeyCodeHolder<T extends Enum<T>> {
 
-    private Class<T> codeClass;
+    private final Class<T> codeClass;
     private T instance;
 
     public ForeignKeyCodeHolder(Class<T> codeClass) {
@@ -29,7 +29,7 @@ public class ForeignKeyCodeHolder<T extends Enum<T>> {
     }
 
     public void setId(Integer id) {
-        this.instance = Codes.fromInt(this.codeClass, id);
+        this.instance = (id == null) ? null : Codes.fromInt(this.codeClass, id);
     }
 
 }
