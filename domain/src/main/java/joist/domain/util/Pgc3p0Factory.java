1b1660bbfd65368bb14fb7021e67fa058e2bdadc
Stephen Haberman <stephen@exigencecorp.com>
2010-08-01 02:28:00 -0500

    domain: Fix protected Pgc3p0Factory constructor.

--- domain/src/main/java/joist/domain/util/Pgc3p0Factory.java
+++ domain/src/main/java/joist/domain/util/Pgc3p0Factory.java
@@ -2,7 +2,7 @@ package joist.domain.util;
 
 public class Pgc3p0Factory extends AbstractC3p0Factory {
 
-    protected Pgc3p0Factory(ConnectionSettings settings) {
+    public Pgc3p0Factory(ConnectionSettings settings) {
         super(settings, "postgresql");
     }
 
