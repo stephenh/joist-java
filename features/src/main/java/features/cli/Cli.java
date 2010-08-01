636e60e703dfbb22fa669d647603142affc7116e
Stephen Haberman <stephen@exigencecorp.com>
2010-07-31 21:01:10 -0500

    features: Don't need the Registry here.

--- features/src/main/java/features/cli/Cli.java
+++ features/src/main/java/features/cli/Cli.java
@@ -5,21 +5,14 @@ import joist.util.SystemProperties;
 
 import org.apache.commons.lang.StringUtils;
 
-import features.Registry;
-
 public class Cli {
 
     public static void main(String[] args) {
         SystemProperties.loadFromFileIfExists("./build.properties");
-        Registry.start();
-        try {
-            for (String arg : args) {
-                String[] parts = StringUtils.split(arg, ".", 2);
-                Object task = Reflection.newInstance("features.cli." + parts[0]);
-                Reflection.invoke(parts[1], task);
-            }
-        } finally {
-            Registry.stop();
+        for (String arg : args) {
+            String[] parts = StringUtils.split(arg, ".", 2);
+            Object task = Reflection.newInstance("features.cli." + parts[0]);
+            Reflection.invoke(parts[1], task);
         }
     }
 
