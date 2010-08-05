f51e6955a4b811297c73e46b05d72aa8ff39d439
Stephen Haberman <stephen@exigencecorp.com>
2010-08-05 10:49:04 -0500

    domain: Refactor AbstractBuilder to not eagerly flush.

--- domain/src/main/java/joist/domain/util/AbstractBuilder.java
+++ domain/src/main/java/joist/domain/util/AbstractBuilder.java
@@ -3,31 +3,25 @@ package joist.domain.util;
 import joist.domain.DomainObject;
 import joist.domain.uow.UoW;
 
+/**
+ * Provides a reference to an instance that, if in acceptance tests,
+ * can load the instance cross units of work to avoid disconnected exceptions.
+ */
 public abstract class AbstractBuilder<T extends DomainObject> {
 
-  // If in integration database mode
-  private Class<T> instanceClass;
-  private Integer instanceId;
-  // If in unit ram mode
   private T instance;
 
   protected AbstractBuilder(T instance) {
-    if (UoW.isOpen()) {
-      if (instance.getId() == null) {
-        UoW.flush();
-      }
-      this.instanceClass = (Class<T>) instance.getClass();
-      this.instanceId = instance.getId();
-    } else {
-      this.instance = instance;
-    }
+    this.instance = instance;
   }
 
   public T get() {
-    if (this.instanceId != null) {
-      return UoW.load(this.instanceClass, this.instanceId);
-    } else {
-      return this.instance;
+    if (UoW.isOpen() && this.instance.getId() != null) {
+      // if the UoW is open, and this instance has been saved already,
+      // ensure our instance is not from a prior UoW (e.g. in tests)
+      this.instance = (T) UoW.load(this.instance.getClass(), this.instance.getId());
     }
+    return this.instance;
   }
+
 }
