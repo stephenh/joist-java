07f552d82f0de623e827f2d88adadfd3d58d599e
Stephen Haberman <stephen@exigencecorp.com>
2011-10-10 17:47:40 -0500

    util: Add joist.sourcegen.defaultIndentation system property.

--- util/src/main/java/joist/sourcegen/GSettings.java
+++ util/src/main/java/joist/sourcegen/GSettings.java
@@ -2,7 +2,7 @@ package joist.sourcegen;
 
 public class GSettings {
 
-  private static String defaultIndentation = "    ";
+  private static String defaultIndentation = System.getProperty("joist.sourcegen.defaultIndentation", "    ");
 
   public static void setDefaultIndentation(String value) {
     defaultIndentation = value;
