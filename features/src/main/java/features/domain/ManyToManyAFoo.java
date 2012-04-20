fdbc832681a412901c7434e570e22edd4038ffb2
Stephen Haberman <stephen@exigencecorp.com>
2012-04-20 13:55:31 -0500

    migrations: Delete join tables when either side is deleted.

--- features/src/main/java/features/domain/ManyToManyAFoo.java
+++ features/src/main/java/features/domain/ManyToManyAFoo.java
@@ -2,4 +2,11 @@ package features.domain;
 
 public class ManyToManyAFoo extends ManyToManyAFooCodegen {
 
+  public ManyToManyAFoo() {
+  }
+
+  public ManyToManyAFoo(String name) {
+    this.setName(name);
+  }
+
 }
