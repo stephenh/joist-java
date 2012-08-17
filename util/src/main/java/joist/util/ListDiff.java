a4f510372d860dfbc9c1b744fee00a609c36a77e
Stephen Haberman <stephen@exigencecorp.com>
2012-08-17 11:03:03 -0500

    util: Fix more NPEs in ListDiff.
    Given the current usage pattern of ListDiff within Joist's domain objects, this
    generally is not expected, but should be handled anyway.
    
    Also, document that it technically treats it's arguments as sets and will not
    handle either containing duplicate values.

--- util/src/main/java/joist/util/ListDiff.java
+++ util/src/main/java/joist/util/ListDiff.java
@@ -6,17 +6,32 @@ import java.util.List;
 
 public class ListDiff<T> {
 
+  /**
+   * Diffs {@code original} and {@code updated}.
+   *
+   * Note that, despite the {@code Collection} types, this generally assumes both
+   * original and updated are sets, because it was originally written for managing
+   * foreign keys of domain objects, where a child is not listed twice.
+   */
   public static <T> ListDiff<T> of(Collection<T> original, Collection<T> updated) {
     List<T> added = new ArrayList<T>();
     List<T> removed = new ArrayList<T>();
-    for (T t : original) {
-      if (!updated.contains(t)) {
-        removed.add(t);
+    if (original == null && updated == null) {
+      // do nothing
+    } else if (original != null && updated == null) {
+      removed.addAll(original);
+    } else if (original == null && updated != null) {
+      added.addAll(updated);
+    } else {
+      for (T t : original) {
+        if (!updated.contains(t)) {
+          removed.add(t);
+        }
       }
-    }
-    for (T t : updated) {
-      if (!original.contains(t)) {
-        added.add(t);
+      for (T t : updated) {
+        if (!original.contains(t)) {
+          added.add(t);
+        }
       }
     }
     return new ListDiff<T>(added, removed);
