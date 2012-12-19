1587ff33016aac63ad62602b0526b49a64022f96
mdietz198 <mark.d.dietz@gmail.com>
2012-12-18 20:59:35 -0600

    features: Add test showing group by with aggregate in select

--- features/src/test/java/features/domain/queries/PrimitivesGroupByTest.java
+++ features/src/test/java/features/domain/queries/PrimitivesGroupByTest.java
@@ -62,6 +62,26 @@ public class PrimitivesGroupByTest extends AbstractFeaturesTest {
     Assert.assertEquals(false, l.get(0).flag.booleanValue());
   }
 
+  @Test
+  public void testGroupByWithAggregateInSelect() {
+    new Primitives("p1").flag(true);
+    new Primitives("p2").flag(true);
+    new Primitives("p3").flag(false);
+    this.commitAndReOpen();
+
+    PrimitivesAlias p = new PrimitivesAlias("p");
+    Select<Primitives> q = Select.from(p);
+    q.select(p.flag.as("flag"), q.count.as("count"));
+    q.groupBy(p.flag);
+    q.orderBy(p.flag.asc());
+    List<ByFlag> l = q.list(ByFlag.class);
+    Assert.assertEquals(2, l.size()); // now two
+    Assert.assertEquals(false, l.get(0).flag.booleanValue());
+    Assert.assertEquals(1L, l.get(0).count.longValue());
+    Assert.assertEquals(true, l.get(1).flag.booleanValue());
+    Assert.assertEquals(2L, l.get(1).count.longValue());
+  }
+
   public static class ByFlag {
     public Boolean flag;
     public Long count;
