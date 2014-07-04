8e00f9cb61541b93c0881df0df290f9610331f68
Stephen Haberman <stephen@exigencecorp.com>
2014-07-04 13:30:16 -0500

    migrations: Generate .leftOn method.

--- features/src/test/java/features/domain/SelectTest.java
+++ features/src/test/java/features/domain/SelectTest.java
@@ -1,5 +1,7 @@
 package features.domain;
 
+import static features.domain.builders.Builders.aParent;
+import static features.domain.builders.Builders.aParentF;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
@@ -7,6 +9,10 @@ import joist.domain.exceptions.TooManyException;
 import joist.domain.orm.queries.Select;
 
 import org.junit.Test;
+
+import features.domain.builders.ChildBuilder;
+import features.domain.builders.ParentBuilder;
+import features.domain.builders.ParentFBuilder;
 
 public class SelectTest extends AbstractFeaturesTest {
 
@@ -37,4 +43,34 @@ public class SelectTest extends AbstractFeaturesTest {
     Select.from(p).where(p.name.eq("parent")).uniqueOrNull();
   }
 
+  @Test
+  public void testJoin() {
+    ParentBuilder parent = aParent().name("p").defaults();
+    ChildBuilder child = parent.newChild().defaults();
+    this.commitAndReOpen();
+
+    ChildAlias c = new ChildAlias();
+    ParentAlias p = new ParentAlias();
+    Child c1 = Select //
+      .from(c)
+      .join(p.on(c.parent))
+      .where(p.name.eq("p"))
+      .uniqueOrNull();
+    assertThat(c1, is(child.get()));
+  }
+
+  @Test
+  public void testLeftJoin() {
+    ParentFBuilder parent = aParentF().defaults();
+    this.commitAndReOpen();
+
+    ParentFAlias p = new ParentFAlias();
+    ChildFAlias c = new ChildFAlias();
+    ParentF p1 = Select //
+      .from(p)
+      .join(c.leftOn(p.childTwo))
+      .where(c.id.isNull().or(c.name.eq("c2")))
+      .uniqueOrNull();
+    assertThat(p1, is(parent.get()));
+  }
 }
