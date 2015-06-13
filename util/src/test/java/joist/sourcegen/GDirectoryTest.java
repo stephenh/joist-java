8bdf8898062f193656403313dbde24ed6bc7fee4
Stephen Haberman <stephen.haberman@gmail.com>
2015-06-13 16:32:25 -0500

    util: Actually fix GDirectoryTest.

--- util/src/test/java/joist/sourcegen/GDirectoryTest.java
+++ util/src/test/java/joist/sourcegen/GDirectoryTest.java
@@ -13,38 +13,48 @@ import org.junit.Test;
 
 public class GDirectoryTest {
 
-  private File foo = new File("./build/org/exigencecorp/Foo.java");
+  private String directory = "./build/GDirectoryTest";
+  private File foo = new File(this.directory + "/org/exigencecorp/Foo.java");
 
   @Before
   public void setUp() {
     if (this.foo.exists()) {
       this.foo.delete();
     }
+    new File(this.directory).mkdir();
   }
 
   @Test
   public void testOutput() throws Exception {
     Assert.assertFalse(this.foo.exists());
 
-    GDirectory bin = new GDirectory("./build");
+    GDirectory bin = new GDirectory(this.directory);
     GClass gc = bin.getClass("org.exigencecorp.Foo");
     gc.getMethod("foo").setBody("int i = 0;");
     bin.output();
 
-    Assert.assertEquals(
-      Join.lines("package org.exigencecorp;", "", "public class Foo {", "", "    public void foo() {", "        int i = 0;", "    }", "", "}", ""),
-      this.read(this.foo));
+    Assert.assertEquals(Join.lines(//
+      "package org.exigencecorp;",
+      "",
+      "public class Foo {",
+      "",
+      "    public void foo() {",
+      "        int i = 0;",
+      "    }",
+      "",
+      "}",
+      ""), this.read(this.foo));
   }
 
   @Test
   public void testMarkTouched() {
-    GDirectory bin = new GDirectory("./bin");
+    GDirectory bin = new GDirectory(this.directory);
     bin.markTouched("org.exigencecorp.Foo");
-    Assert.assertEquals(Copy.list(new File("./bin/org/exigencecorp/Foo.java")), bin.getTouched());
-    Assert.assertEquals(Copy.list(new File("./bin/org/exigencecorp")), bin.getUsedDirectories());
+    Assert.assertEquals(Copy.list(new File(this.directory + "/org/exigencecorp/Foo.java")), bin.getTouched());
+    Assert.assertEquals(Copy.list(new File(this.directory + "/org/exigencecorp")), bin.getUsedDirectories());
     Assert.assertEquals(Copy.list(//
-      new File("./bin/org"),
-      new File("./bin/org/exigencecorp")), bin.getAllDirectories());
+      new File(this.directory + "/org"),
+      new File(this.directory + "/org/exigencecorp")), bin.getAllDirectories());
   }
 
   private String read(File file) throws Exception {
